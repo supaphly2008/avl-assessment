2e4210ac7080f7d508ae6eb26e19246fdefdc3f3
Terry <liaoiyang@gmail.com>
2022-03-21 00:09:54 +0800

    setup input range

--- components/Search.js
+++ components/Search.js
@@ -1,12 +1,20 @@
 import { useState } from "react";
 import Input from "./common/Input";
 import Button from "./common/Button";
+import Slider from "./common/Slider";
+
+const inputRange = [3, 6, 9, 12, 15, 50];
 
 const Search = () => {
   const [searchText, setSearchText] = useState("");
+  const [range, setRange] = useState(0);
+
+  const onRangeChange = (e) => {
+    setRange(inputRange[e.target.value]);
+  };
 
   const onSearchClick = () => {
-    console.log("click");
+    console.log("click", range);
   };
 
   return (
@@ -21,6 +29,7 @@ const Search = () => {
         <div className="text-[48px] font-bold">
           30 <span className="text-[16px] font-normal tracking-[0.15px]">Results</span>
         </div>
+        <Slider range={inputRange} onChange={onRangeChange} />
       </div>
       <hr className="mt-[30px] text-gray" />
       <Button className="absolute bottom-0" label="search" onClick={onSearchClick} />
