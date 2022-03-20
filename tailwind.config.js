6617374a7f7ee52136c78e97ae7c40421769253d
Terry <liaoiyang@gmail.com>
2022-03-20 16:03:22 +0800

    add home page

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,8 @@ module.exports = {
       xs: { max: "375px" },
     },
     colors: {
-      black: "#000000",
+      black: "#121212",
+      "black--lighter": "#1B1B1B",
       white: "#ffffff",
       orange: "#FF9B33",
       gray: "#5d5d5d",
