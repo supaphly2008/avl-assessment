abd29ef1f1dddecd6257487dcd9a3b4cdddf6019
Terry Liao <terry.liao@nogle.com>
2022-03-22 16:49:03 +0800

    search page input debounce and execute api as user types

--- components/common/Button.js
+++ components/common/Button.js
@@ -1,12 +1,12 @@
-const Button = ({ label, onClick, className = "", isUppercase = true }) => {
+const Button = ({ label, onClick, className = "", isUppercase = true, isDisabled = false }) => {
   const upperCase = isUppercase ? "uppercase" : "";
-  const hoverClass = "hover:border hover:border-white hover:bg-transparent hover:text-white";
+  const hoverClass = isDisabled ? "disabled:cursor-not-allowed opacity-20" : "hover:border hover:border-white hover:bg-transparent hover:text-white";
   const baseClass = "inline-block h-[40px] w-[335px] text-black cursor-pointer text-center leading-none px-[12px] py-[13px] rounded-[4px] bg-white text-[14px] font-bold duration-300";
 
   return (
-    <div className={`${className} ${upperCase} ${hoverClass} ${baseClass}`} onClick={onClick}>
+    <button className={`${className} ${upperCase} ${baseClass} ${hoverClass}`} disabled={isDisabled} onClick={onClick}>
       {label}
-    </div>
+    </button>
   );
 };
 
--- components/search/Search.js
+++ components/search/Search.js
@@ -1,8 +1,9 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Input from "../common/Input";
 import Button from "../common/Button";
 import Slider from "../common/Slider";
 import { CompState } from "./index";
+import useDebounce from "../../hooks/useDebounce";
 
 import { getSearch } from "../../api";
 
@@ -10,24 +11,47 @@ const inputRange = [3, 6, 9, 12, 15, 50];
 
 const Search = ({ setComponent, setSearchResult, searchResult }) => {
   const [searchText, setSearchText] = useState("");
-  const [range, setRange] = useState(0);
+  const [range, setRange] = useState(inputRange[0]); // page size: ;
+  const debouncedSearchTerm = useDebounce(searchText, 500);
+  const [buttonDisabled, setButtonDisabled] = useState(false);
+  const [isSearching, setIsSearching] = useState(false); // is searching (loading)
 
+  useEffect(async () => {
+    if (debouncedSearchTerm) {
+      const result = await getSearch(1, range, debouncedSearchTerm);
+      setSearchResult(result);
+    } else {
+      setSearchResult([]);
+    }
+  }, [debouncedSearchTerm, range]);
+
+  // input range change
   const onRangeChange = (e) => {
     setRange(inputRange[e.target.value]);
   };
 
   const onSearchClick = async () => {
-    console.log("click", range);
-    const result = await getSearch();
-    setSearchResult(result);
     setComponent(CompState.SEARCH_RESULT);
   };
+
+  // input text change
+  const onInputChange = (e) => {
+    setSearchText(e.target.value);
+  };
+
+  useEffect(() => {
+    if (searchResult.data?.total === 0 || !debouncedSearchTerm) {
+      setButtonDisabled(true);
+    } else {
+      setButtonDisabled(false);
+    }
+  }, [searchResult]);
 
   return (
     <div className="relative h-full">
       <>
         <div className="mb-[20px] text-[24px]">Search</div>
-        <Input placeholder="Keyword" value={searchText} onChange={(e) => setSearchText(e.target.value)} />
+        <Input placeholder="Keyword" value={searchText} onChange={onInputChange} />
       </>
       <hr className="my-[30px] text-gray" />
       <div>
@@ -38,7 +62,7 @@ const Search = ({ setComponent, setSearchResult, searchResult }) => {
         <Slider range={inputRange} onChange={onRangeChange} />
       </div>
       <hr className="mt-[30px] text-gray" />
-      <Button className="absolute bottom-0" label="search" onClick={onSearchClick} />
+      <Button isDisabled={buttonDisabled} className="absolute bottom-0" label="search" onClick={onSearchClick} />
     </div>
   );
 };
