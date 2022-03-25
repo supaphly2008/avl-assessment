1e734b357709035284c602f391517a914cd959b2
Terry Liao <terry.liao@nogle.com>
2022-03-25 11:51:06 +0800

    add tag to tags component

--- components/tags/index.js
+++ components/tags/index.js
@@ -1,7 +1,24 @@
+import Tag from "./Tag";
+
 const Tags = () => {
   return (
-    <div>
+    <div className="h-full flex flex-col">
       <div className="text-[30px]">Tags</div>
+      <div className="mt-[24px] flex flex-wrap gap-x-[24px] gap-y-[36px] overflow-auto">
+        <Tag tagText="Tag" title="title" desc="234 Results" />
+        <Tag tagText="Tag" title="title" desc="234 Results" />
+        <Tag tagText="Tag" title="title" desc="234 Results" />
+        <Tag tagText="Tag" title="title" desc="234 Results" />
+        <Tag tagText="Tag" title="title" desc="234 Results" />
+        <Tag tagText="Tag" title="title" desc="234 Results" />
+        <Tag tagText="Tag" title="title" desc="234 Results" />
+        <Tag tagText="Tag" title="title" desc="234 Results" />
+        <Tag tagText="Tag" title="title" desc="234 Results" />
+        <Tag tagText="Tag" title="title" desc="234 Results" />
+        <Tag tagText="Tag" title="title" desc="234 Results" />
+        <Tag tagText="Tag" title="title" desc="234 Results" />
+        <Tag tagText="Tag" title="title" desc="234 Results" />
+      </div>
     </div>
   );
 };
