116d1f39fe9f603d50e97b52455d7f0efa016b55
Terry <liaoiyang@gmail.com>
2022-03-26 23:35:06 +0800

    fix height

--- pages/index.js
+++ pages/index.js
@@ -43,7 +43,7 @@ export default function Home() {
   };
 
   return (
-    <div className="flex h-screen">
+    <div className="flex h-full">
       {ShowContent()}
       {/* followers */}
       {width > hideFollowsWidth && (
