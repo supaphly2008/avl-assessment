2d46d505580fce858823b5988c1607a9519d2be9
Terry <liaoiyang@gmail.com>
2022-04-01 15:53:47 +0800

    adjust height

--- components/common/ImageCard.js
+++ components/common/ImageCard.js
@@ -1,7 +1,7 @@
 const ImageCard = ({ title = "title", username = "username", src = "", alt = "" }) => {
   return (
     <div>
-      <img className="max-h-[335px] min-h-[146px] w-full" src={src} alt={alt} />
+      <img className="max-h-[335px] min-h-[146px] w-full xs:max-h-[222.64px]" src={src} alt={alt} />
       <div className="mt-[12px] text-[14.9px]">{title}</div>
       <div className="text-[11.17px] text-gray--lighter">{username}</div>
     </div>
